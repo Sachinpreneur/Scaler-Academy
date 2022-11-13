e8fb9b2d1b9564d6871920216dec20162539dd29
Sachinpreneur <sachinpreneur16@gmail.com>
2022-11-13 16:37:24 +0530

    first code

---
+++ Scaler_1.py
@@ -0,0 +1,13 @@
+# numbers, integers, float, complex
+
+square_area = 25
+circle_area = 65
+iota = (-1) ** (1/2)
+
+print(square_area, circle_area, iota) 
+
+# Mathamatical operatos
+
+print(square_area + circle_area)
+print(4 + circle_area)
+print(iota / square_area)
